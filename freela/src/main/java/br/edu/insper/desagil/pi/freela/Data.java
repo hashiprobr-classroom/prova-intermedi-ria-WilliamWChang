45b9887db1a028a2aed837d77115c63381d0c875
WilliamWChang <146737637+WilliamWChang@users.noreply.github.com>
2025-09-29 09:28:25 -0300

    teste

---
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/Data.java
@@ -0,0 +1,25 @@
+public class Data extends Momento{
+    private int ano - 1970;
+    private int mes - 1;
+    private int dia - 1;
+
+    private Map<Interger,Interger>
+
+
+
+    Map<Integer, Integer> limite = new HashMap<>();
+limite.put(1, 31);
+limite.put(2, 28);
+limite.put(3, 31);
+limite.put(4, 30);
+limite.put(5, 31);
+limite.put(6, 30);
+limite.put(7, 31);
+limite.put(8, 31);
+limite.put(9, 30);
+limite.put(10, 31);
+limite.put(11, 30);
+limite.put(12, 31);
+this.limite = limite;
+
+}
---
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/Momento.java
@@ -0,0 +1,15 @@
+public abstract class Momento {
+    public int ajusta(int valor, int minimo , int maximo){
+        if (valor < minimo) {
+            return minimo;
+        }
+        else if (valor > maximo) {
+            return maximo;
+        }
+        else {
+            return valor;
+        }
+    }
+
+    public abstract int minutos();
+}
